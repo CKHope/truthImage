4b2e54668edfe8cfd1cc478a951fac44b8290a87
CKHope <chungkien20211011@gmail.com>
2023-07-12 01:03:06 +0700

    ud

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import sample, choice
 import os
 
 
-def generate_file_path(truthType: str, levels: int = 3, fileNameLen: int = 15, tag=""):
+def generate_file_path(truthType: str, levels: int = 3, fileNameLen: int = 15, tag="#"):
     today = str(datetime.now().strftime("%d%m%y"))
     folders = [choice(string.ascii_lowercase) for _ in range(levels)]
     # folder_path = './' + '/'.join(folders) + '/'
@@ -14,7 +14,7 @@ def generate_file_path(truthType: str, levels: int = 3, fileNameLen: int = 15, t
     letters = string.ascii_letters
     fileName = "".join(choice(letters) for _ in range(fileNameLen))
 
-    file_path = f"{truthType}/{today}/{folder_path}link{fileName}xxx{tag}.md"
+    file_path = f"{truthType}/{today}/{folder_path}h{fileName}{tag}.md"
     return file_path
 
 
@@ -27,8 +27,13 @@ def generate_markdown_file(length):
     markdown_content = """\
 # Displaying Images
 
-![Image 1](./im/1.jpg)
-![Image 2](./im/2.jpg)
+<div style="text-align: center;">
+    <img src="./im/1.jpg" alt="Image 1" />
+</div>
+
+<div style="text-align: center;">
+    <img src="./im/2.jpg" alt="Image 2" />
+</div>
 """
 
     # Save the Markdown content to a file
